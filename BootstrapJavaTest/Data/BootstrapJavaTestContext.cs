623bb79be4c7c48485a1b40c3f2f31083304fbc6
ramious76 <8411220+ramious76@users.noreply.github.com>
2018-06-19 19:24:25 -0700

    Initial commit

---
+++ BootstrapJavaTest/Data/BootstrapJavaTestContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BootstrapJavaTest.Models
+{
+    public class BootstrapJavaTestContext : DbContext
+    {
+        public BootstrapJavaTestContext (DbContextOptions<BootstrapJavaTestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BootstrapJavaTest.Models.Person> Person { get; set; }
+    }
+}
---
+++ BootstrapJavaTest/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootstrapJavaTest.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public int Age { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+    }
+}
